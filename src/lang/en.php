c88a99feb39473b4e5fdfbe1fb17ee2a2fb443fa
Truncheon-eng <146008203+Truncheon-eng@users.noreply.github.com>
2025-02-05 20:21:47 +0000

    added rus.php and en.php

---
+++ src/lang/en.php
@@ -0,0 +1,4 @@
+<?php
+$text = "<h2>About our project.</h2><p>Our project is an innovative platform designed to simplify interaction between users and provide convenient access to important resources. We strive to make the process as simple and intuitive as possible so that every user can quickly find what they need.<br>
+The main goal of the project is to bring people together by providing them with tools to effectively solve everyday tasks. We pay special attention to security and convenience to ensure that your data remains protected and that working with the platform brings only positive emotions.</p>";
+?>
